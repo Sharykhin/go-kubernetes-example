adb34d07c24d671888909d0e7e147b1dd2e042b6
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2018-04-03 14:34:48 +0300

    asdasdasas

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello World!!!"))
+		w.Write([]byte("Hello World."))
 	})
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
