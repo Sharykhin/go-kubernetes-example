9d19b7f3c3e5eccaf891e571ea1ad5a9ac5d32d5
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2018-04-09 10:46:40 +0300

    working on migrate

--- command/migrate.go
+++ command/migrate.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 )
 
 func main() {
 	fmt.Println(os.Getenv("DB_USER"))
+	cmd := exec.Command("echo", `"${DB_PASS}"`)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("run:", out.String())
 }
