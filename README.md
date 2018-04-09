71a218204f7b48adfe9afb8380aec5c244bab3bf
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2018-04-09 11:01:09 +0300

    fix migratons

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ kubectl create clusterrolebinding --user system:serviceaccount:kube-system:defau
 MYSQL_ADDRESS="root:root@tcp(mysql-service-2:3306)/test?charset=utf8"
 ```
 
+Example of using sed
+```bash
+sed 's/${VERSION}/0.0.1/g' deployment.tpl.yml > deployment.yml
+```
--- command/migrate.go
+++ command/migrate.go
@@ -10,12 +10,15 @@ import (
 
 func main() {
 	fmt.Println(os.Getenv("DB_USER"))
-	cmd := exec.Command("echo", `"${DB_PASS}"`)
+
+	address := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	cmd := exec.Command("goose", "-dir", "migrations", "mysql", address, "up")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("run:", out.String())
+	fmt.Println("Migrate: ", out.String())
 }
