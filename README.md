ecf11441ee904b2d7944086c6e09fe22284559b8
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2018-04-09 11:12:45 +0300

    asdas

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ MYSQL_ADDRESS="root:root@tcp(mysql-service-2:3306)/test?charset=utf8"
 
 Example of using sed
 ```bash
-sed 's/${VERSION}/0.0.1/g' deployment.tpl.yml > deployment.yml
+sed 's/${VERSION}/'"${VERSION}"'/g' deployment.tpl.yml > deployment.yml
 ```
--- command/migrate.go
+++ command/migrate.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	"bytes"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/mattes/migrate"
+	"github.com/mattes/migrate/database/mysql"
+	_ "github.com/mattes/migrate/source/file"
 	"log"
 	"os"
-	"os/exec"
 )
 
 func main() {
 	fmt.Println(os.Getenv("DB_USER"))
 
-	address := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
-
-	cmd := exec.Command("goose", "-dir", "migrations", "mysql", address, "up")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/test?parseTime=true", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST")))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Migrate: ", out.String())
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://../migrations",
+		"mysql", driver)
+	m.Up()
 }
