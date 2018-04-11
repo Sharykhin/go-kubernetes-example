29dd3b1ab3e67912afb6121f5bd1bfaf297e2db8
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2018-04-11 11:05:56 +0300

    asdas

--- goose_test/00002_add_column.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
--- SQL in this section is executed when the migration is applied.
-ALTER TABLE test ADD COLUMN first_name VARCHAR(80);
-
--- +goose Down
--- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE test DROP COLUMN first_name;
